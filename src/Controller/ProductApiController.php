9dbf368523a10349f535044435a15bb621b9857a
Gabijus Kielius <gabijuskielius@gmail.com>
2020-08-30 15:13:06 +0300

    feat: Added meteo request

--- src/Controller/ProductApiController.php
+++ src/Controller/ProductApiController.php
@@ -2,22 +2,38 @@
 
 namespace App\Controller;
 
+use App\Service\WeatherProductRecommendationService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class ProductApiController extends AbstractController
 {
     /**
      * @Route("/api/products/{city}", requirements={"city": "^\w+$"}, name="product_api", methods={"POST", "GET"})
      */
-    public function index(string $city)
+    public function index(string $city, HttpClientInterface $client, WeatherProductRecommendationService $weatherProductRecommendationService)
     {
-        dump($city);
+        $response = $client->request(
+            'GET',
+            "https://api.meteo.lt/v1/places/${city}/forecasts/long-term"
+        );
+
+        //TODO: Check if there is content
+
+        //TODO: if there is then find recommended products
+
+        /** @var array $products */
+        $products = $weatherProductRecommendationService->getRecommendedProductsFromWeatherData($response->getContent());
+
+        //TODO: else return response with errror
+
 
         return new JsonResponse(
             [
-                "city" => $city
+                "city" => $city,
+                "products" => $products,
             ]
         );
     }
