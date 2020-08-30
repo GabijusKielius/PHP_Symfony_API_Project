5499013d04e6eeb9c184b2a16e86e8b5911bd7e6
Gabijus Kielius <gabijuskielius@gmail.com>
2020-08-30 15:12:02 +0300

    feat: Added WeatherProductRecommendationService.php service

---
+++ src/Service/WeatherProductRecommendationService.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Service;
+
+
+class WeatherProductRecommendationService
+{
+    public function getRecommendedProductsFromWeatherData(array $weatherData)
+    {
+        // TODO: Return JSON Of recommended products
+
+        return null;
+    }
+}
