d44b34e213ffe30a33a2cef2065804c600eed8c4
Gabijus Kielius <gabijuskielius@gmail.com>
2020-08-30 13:31:41 +0300

    feat: Added faker support

--- src/DataFixtures/ProductFixtures.php
+++ src/DataFixtures/ProductFixtures.php
@@ -2,19 +2,27 @@
 
 namespace App\DataFixtures;
 
+use Faker\Factory;
 use App\Entity\Product;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class ProductFixtures extends Fixture
 {
+    private static $productNames = [
+        'Red Hat',
+        'Black Umbrella',
+        'Big boy boots'
+    ];
+
     public function load(ObjectManager $manager)
     {
-         $product = new Product();
-         $product->setName("testas");
-         $product->setSku("TS_01");
-         $product->setPrice(5.50);
-         $manager->persist($product);
+        $faker = Factory::create();
+        $product = new Product();
+        $product->setName($faker->randomElement(self::$productNames));
+        $product->setSku($faker->uuid);
+        $product->setPrice($faker->randomFloat(2,0.01,100000));
+        $manager->persist($product);
 
         $manager->flush();
     }
