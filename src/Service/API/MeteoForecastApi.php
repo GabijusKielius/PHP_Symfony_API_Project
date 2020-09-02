9fcec06e181f04536d0cb748620d785954759742
Gabijus Kielius <gabijuskielius@gmail.com>
2020-09-02 21:51:21 +0300

    feat: Added weather condition enum function

--- src/Service/API/MeteoForecastApi.php
+++ src/Service/API/MeteoForecastApi.php
@@ -4,6 +4,7 @@
 namespace App\Service\API;
 
 
+use App\Enum\WeatherConditionEnum;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class MeteoForecastApi implements ForecastApiInterface
@@ -12,6 +13,9 @@ class MeteoForecastApi implements ForecastApiInterface
      * @var HttpClientInterface
      */
     private $client;
+
+    public $forecastTime = "forecastTimeUtc";
+    public $forecastConditionType = "conditionCode";
 
     public function __construct(HttpClientInterface $client)
     {
@@ -25,6 +29,39 @@ class MeteoForecastApi implements ForecastApiInterface
             "https://api.meteo.lt/v1/places/${city}/forecasts/long-term"
         );
 
-        return json_decode($response->getContent(), true);
+        return json_decode($response->getContent(), true)['forecastTimestamps'];
+    }
+
+    public function getWeatherConditionEnum(string $conditionType)
+    {
+        switch ($conditionType)
+        {
+            case 'clear':
+                return WeatherConditionEnum::clear;
+            case 'isolated-clouds':
+                return WeatherConditionEnum::isolated_clouds;
+            case 'scattered-clouds':
+                return WeatherConditionEnum::scattered_clouds;
+            case 'overcast':
+                return WeatherConditionEnum::overcast;
+            case 'light-rain':
+                return WeatherConditionEnum::light_rain;
+            case 'moderate-rain':
+                return WeatherConditionEnum::moderate_rain;
+            case 'heavy-rain':
+                return WeatherConditionEnum::heavy_rain;
+            case 'sleet':
+                return WeatherConditionEnum::sleet;
+            case 'light-snow':
+                return WeatherConditionEnum::light_snow;
+            case 'moderate-snow':
+                return WeatherConditionEnum::moderate_snow;
+            case 'heavy-snow':
+                return WeatherConditionEnum::heavy_snow;
+            case 'fog':
+                return WeatherConditionEnum::fog;
+            case 'na':
+                return WeatherConditionEnum::na;
+        }
     }
 }
