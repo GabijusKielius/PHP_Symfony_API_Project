ebd3455a92c9a75916735538eaa4cd2ab903dd5d
Gabijus Kielius <gabijuskielius@gmail.com>
2020-09-02 21:54:43 +0300

    feat: Added database product query

--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -19,32 +19,15 @@ class ProductRepository extends ServiceEntityRepository
         parent::__construct($registry, Product::class);
     }
 
-    // /**
-    //  * @return Product[] Returns an array of Product objects
-    //  */
-    /*
-    public function findByExampleField($value)
+    public function findAllProductsByWeatherCondition(int $weatherCondition)
     {
         return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('p.id', 'ASC')
-            ->setMaxResults(10)
+            ->andWhere('p.weatherCondition = :weather_condition')
+            ->setParameter('weather_condition', $weatherCondition)
+            ->setMaxResults(2)
             ->getQuery()
-            ->getResult()
+            ->getArrayResult()
         ;
     }
-    */
 
-    /*
-    public function findOneBySomeField($value): ?Product
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
