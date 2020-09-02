c484367389aadffc109dbf2e735ca269b52bf1c7
Gabijus Kielius <gabijuskielius@gmail.com>
2020-09-02 21:54:24 +0300

    refactor: JSON response structure changed

--- src/Controller/ProductApiController.php
+++ src/Controller/ProductApiController.php
@@ -7,6 +7,7 @@ use App\Service\WeatherProductRecommendationService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class ProductApiController extends AbstractController
@@ -14,19 +15,25 @@ class ProductApiController extends AbstractController
     /**
      * @Route("/api/products/{city}", requirements={"city": "^\w+$"}, name="product_api", methods={"POST", "GET"})
      */
-    public function index(string $city,MeteoForecastApi $forecastApi,WeatherProductRecommendationService $weatherProductRecommendationService)
-    {
-        try {
-            $weatherData = $forecastApi->getForecastDataInArray($city);
-            $responseContent = $weatherProductRecommendationService->getRecommendedProductsFromWeatherData($weatherData);
-        }catch (\Exception $e){
-            $responseContent = ['error'=> $e->getMessage()];
-        }
+    public function index(
+        string $city,
+        MeteoForecastApi $forecastApi,
+        WeatherProductRecommendationService $weatherProductRecommendationService
+    ) {
 
+        try{
+        $weatherData = $forecastApi->getForecastDataInArray($city);
+        $recommendations = $weatherProductRecommendationService->getRecommendedProductsFromWeatherData($weatherData);
+        }
+        catch(\Exception $e){
+            $recommendations = ['not found'];
+        }
         return new JsonResponse(
             [
-                $responseContent
+                "city"=>$city,
+                "recommendations" => $recommendations
             ]
         );
+
     }
 }
